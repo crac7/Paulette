3c4d450ac9992662352482983cae2dc59787f1e1
crac7 <cesaranzules7@hotmail.com>
2019-01-06 23:16:14 -0500

    agregue lineas

--- dibujar.js
+++ dibujar.js
@@ -1,9 +1,27 @@
 var d = document.getElementById("dibujo");
 var lienzo = d.getContext("2d");
+var lineas= 30;
+var l= 0;
+var yi, xf;
+var colorcito ="#FAA";
 
-lienzo.beginPath();
-lienzo.strokeStyle ="red";
-lienzo.moveTo(50,150);
-lienzo.lineTo(100,100);
-lienzo.stroke();
-lienzo.closePath();
+for(l=0; l < lineas;l++)
+{
+   yi=  10*l;
+   xf = 10 * (l + 1);
+   dibujarLinea(colorcito ,300,yi,xf,0);
+   console.log("Linea "+ l);
+
+}
+dibujarLinea(colorcito,299,299,299,1);
+dibujarLinea(colorcito,299,1,1,1);
+
+function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial,yinicial);
+  lienzo.lineTo(xfinal,yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
